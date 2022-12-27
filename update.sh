a10be061de0d423e6567c6151dbd519ccdf27536
Patryk Palej <patrykpalej@gmail.com>
2022-12-27 22:51:43 +0100

    Just another update

---
+++ update.sh
@@ -0,0 +1,9 @@
+source venv/bin/activate
+python automations/generate_toc.py
+python automations/generate_content.py
+python automations/yaml_to_json.py
+
+
+git add -A
+git commit -m "Just another update"
+git push
